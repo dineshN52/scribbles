f13bf1397e554878967cb5f48e7b69bb27ba16ff
Dinesh N <dinesh.n@metamation.com>
2024-02-21 12:25:35 +0530

    Scribble pad

---
+++ scribbles/CodeFile1.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace scribbles;
+
+internal partial class MyCanvas : Canvas {
+    System.Windows.Point mCurrentpoint = new ();
+    readonly List<System.Windows.Point> mPoints = new ();
+    Stack<List<System.Windows.Point>> mDStrokes = new ();
+    Stack<List<System.Windows.Point>> undoStrokes = new ();
+    public Stack<List<System.Windows.Point>> Strokes => mDStrokes;
+    public bool IsErase { get; set; }
+    protected override void OnRender (DrawingContext dc) {
+        base.OnRender (dc);
+        Pen pen = new (Brushes.Black, 1);
+        if (mDStrokes != null) {
+            Stack<List<System.Windows.Point>> strokes = new ();
+            foreach (var s in mDStrokes)
+                strokes.Push (s);
+            int c = strokes.Count;
+            for (int i = 0; i < c; i++) {
+                List<System.Windows.Point> points = strokes.Pop ();
+                for (int j = 0; j < points.Count - 1; j++)
+                    dc.DrawLine (pen, points[j], points[j + 1]);
+            }
+        }
+        if (mPoints.Count > 1) {
+            for (int i = 0; i < mPoints.Count - 1; i++)
+                dc.DrawLine (pen, mPoints[i], mPoints[i + 1]);
+        }
+    }
+
+    protected override void OnMouseDown (MouseButtonEventArgs e) {
+        if (e.LeftButton == MouseButtonState.Pressed) {
+            mCurrentpoint = e.GetPosition (this);
+            mPoints.Add (mCurrentpoint);
+        }
+    }
+
+    protected override void OnMouseMove (MouseEventArgs e) {
+        if (e.LeftButton != MouseButtonState.Pressed) return;
+        System.Windows.Point p = e.GetPosition (this);
+        mPoints.Add (p);
+        InvalidateVisual ();
+    }
+
+    protected override void OnMouseLeftButtonUp (MouseButtonEventArgs e) {
+        if (e.ButtonState == MouseButtonState.Released) {
+            List<System.Windows.Point> p = new ();
+            foreach (var element in mPoints)
+                p.Add (element);
+            mDStrokes.Push (p);
+            mPoints.Clear ();
+            InvalidateVisual ();
+        }
+    }
+
+    public void Undo() {
+        if (mDStrokes.Count == 0) return;
+        undoStrokes.Push (mDStrokes.Pop ());
+        InvalidateVisual ();
+    }
+    public void Redo() {
+        if (undoStrokes.Count == 0) return;
+        mDStrokes.Push (undoStrokes.Pop ());
+        InvalidateVisual ();
+    }
+}
+
+
---
+++ scribbles/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.Win32;
+using System.Text;
+
+namespace scribbles {
+
+    /// <summary>Interaction logic for MainWindow.xaml</summary>
+    public partial class MainWindow : Window {
+        readonly Microsoft.Win32.OpenFileDialog mOpen = new ();
+
+        public MainWindow () {
+            InitializeComponent ();
+        }
+
+        /// <summary>Method to create a new window every new file</summary>
+        /// <param name="e"></param>
+        private void NewWindow_Click (object sender, RoutedEventArgs e) {
+            MainWindow w = new ();
+            w.Show ();
+        }
+        private void Exit_Click (object sender, RoutedEventArgs e) => Close ();
+
+        private void OpenFile_Click (object sender, RoutedEventArgs e) {
+            mOpen.DefaultExt = ".png";
+            mOpen.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+        }
+
+        private void SaveAsTxt_Click (object sender, RoutedEventArgs e) {
+            StringBuilder newFile = new ();
+            foreach (var file in canvas.Strokes) {
+                foreach (var points in file)
+                    newFile.AppendLine (points.ToString ());
+                _ = newFile.Append ('\n');
+            }
+            string fileText = newFile.ToString ();
+            SaveFileDialog dialog = new () {
+                Filter = "Text Files(*.txt)|*.txt|All(*.*)|*"
+            };
+            if (dialog.ShowDialog () == true)
+                File.WriteAllText (dialog.FileName, fileText);
+        }
+
+        private void SaveAsBin_Click (object sender, RoutedEventArgs e) {
+            SaveFileDialog dialog = new () {
+                Filter = "Binary Files(*.bin)|*.bin|All(*.*)|*"
+            };
+            if (dialog.ShowDialog () == true) {
+                using var s = File.OpenWrite (dialog.FileName);
+                var bw = new BinaryWriter (s);
+                foreach (var file in canvas.Strokes) {
+                    foreach (var streak in file) {
+                        bw.Write ((byte)streak.X);
+                        bw.Write ((byte)streak.Y);
+                        bw.Write (default(byte));
+                    }
+                    bw.Write (Encoding.ASCII.GetBytes ("n"));
+                }
+            }
+        }
+
+        private void Save_Click (object sender, RoutedEventArgs e) {
+
+        }
+        private void MenuItem_Click (object sender, RoutedEventArgs e) {
+            OpenFileDialog open = new ();
+            if (open.ShowDialog () == true) {
+                byte[] fileBytes = File.ReadAllBytes (open.FileName);
+                List<List<byte>> stride = new ();
+                List<byte> str = new ();
+                foreach(var b in fileBytes) {                 
+                    byte n= Encoding.ASCII.GetBytes ("n")[0];                
+                    if(b != n) {
+                        str.Add (b);continue;
+                    }
+                    stride.Add (new List<byte> (str));
+                    str.Clear ();
+                }         
+            }
+        }
+
+        private void EraseButton_Click (object sender, RoutedEventArgs e) => canvas.IsErase = canvas.IsErase != true;
+
+        private void Undo_Click (object sender, RoutedEventArgs e) => canvas.Undo ();
+
+        private void Button_Click (object sender, RoutedEventArgs e) => canvas.Redo ();
+    }
+}
